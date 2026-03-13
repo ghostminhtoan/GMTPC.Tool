0be160eb4ac74aa7cc21d292740ea75f6f3d239e
ghostminhtoan <ghostminhtoan@gmail.com>
2026-03-13 18:39:21 +0700

    "CreateMainWindowSystemDownload_CentralizeDownloadLogic

--- Generated/BuildNumber.cs
+++ Generated/BuildNumber.cs
@@ -3,6 +3,6 @@ namespace GMTPC.Tool
 { 
     public static class BuildInfo 
     { 
-        public const string BUILD_NUMBER = "2026-03-13-18-24-07"; 
+        public const string BUILD_NUMBER = "2026-03-13-18-38-29"; 
     } 
 }
