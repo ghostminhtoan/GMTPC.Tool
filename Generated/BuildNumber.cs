96a28763b58bc1866698a9ed242fbeee963c29c4
ghostminhtoan <ghostminhtoan@gmail.com>
2026-03-16 11:54:02 +0700

    [UI] Reduce TabItem header font size to 11px to prevent wrapping

--- Generated/BuildNumber.cs
+++ Generated/BuildNumber.cs
@@ -3,6 +3,6 @@ namespace GMTPC.Tool
 { 
     public static class BuildInfo 
     { 
-        public const string BUILD_NUMBER = "2026-03-16 / 09:43:59"; 
+        public const string BUILD_NUMBER = "2026-03-16 / 11:53:33"; 
     } 
 }
