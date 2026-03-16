dd654e778b13c3526248373b477888be7062ce03
Ma Minh Toan <ghostminhtoan@gmail.com>
2026-03-17 06:27:12 +0700

    [UI] Move Temp folder combobox next to Select All button

--- Generated/BuildNumber.cs
+++ Generated/BuildNumber.cs
@@ -3,6 +3,6 @@ namespace GMTPC.Tool
 { 
     public static class BuildInfo 
     { 
-        public const string BUILD_NUMBER = "2026-03-17 / 06:20:08"; 
+        public const string BUILD_NUMBER = "2026-03-17 / 06:27:01"; 
     } 
 }
